3c7ac437e9b55c9beffbf0713ea849084bb30bcd
Fatima Goncalves <124080552+cx-fatima-goncalves@users.noreply.github.com>
2026-07-06 13:57:07 +0100

    Update Program.cs

--- SQLi_1/Program.cs
+++ SQLi_1/Program.cs
@@ -16,12 +16,9 @@ namespace SQLi_1
                 var user = args[0];
                 var pwd = Encrypt(args[1]);
                 Login(user, pwd);
-				var password = "1!.Acjjjj";
-				var password2 = "1!.Acjjjj";
             }
             catch  
             {
-
                 Console.WriteLine("An error has occurred !!");
             }
             
